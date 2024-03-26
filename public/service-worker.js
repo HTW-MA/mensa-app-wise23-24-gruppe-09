1da38caace2c4bdc4b8141a67ceb76ac1a4b5c7d
s0568841 <s0568841@htw-berlin.de>
2024-03-26 20:58:57 +0100

    basic serviceworker implementiert

---
+++ public/service-worker.js
@@ -0,0 +1,13 @@
+// service-worker.js
+self.addEventListener('install', event => {
+    console.log('Service Worker installed');
+});
+
+self.addEventListener('activate', event => {
+    console.log('Service Worker activated');
+});
+
+self.addEventListener('fetch', event => {
+    console.log('Service Worker fetching:', event.request.url);
+    event.respondWith(fetch(event.request));
+});
