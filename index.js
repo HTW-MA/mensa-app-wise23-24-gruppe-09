0f845cf4acded991fe54b042b855e1b0ce35c160
s0568841 <s0568841@htw-berlin.de>
2024-04-04 23:30:48 +0200

    removed using https from the index.js

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
 // index.js
-//auswählen des Express frameworks
 const express = require('express');
-//auswählen des Internet Protokolls
-const https = require('https');
-const fs = require('fs');
-
-
-//sicherstellung von CORS funktionen, damit das frontend auf das backend zugreifen kann
 const cors = require('cors');
-const {queryDatabase} = require ('./src/database/userDB');
+const { queryDatabase } = require('./src/database/userDB');
 const { registerUserService } = require('./src/service/userService');
 const app = express();
 const port = process.env.PORT || 3001;
-// Verwende JSON
 app.use(express.json());
 
-// Importiere die Route für "/mensas", "/meals" und "/additives"
+// Import the routes
 const mensasRoute = require('./src/routes/mensaRoutes');
 const mealsRoute = require('./src/routes/meal.routes');
 const additivesRoute = require('./src/routes/additives.routes');
 
-//Verwendung von CORS, damit das Frontend auf das Backend zugreifen kann
+// Enable CORS
 app.use(cors({
     origin: 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // enable set cookie
+    credentials: true,
     optionsSuccessStatus: 204,
 }));
 
@@ -39,25 +31,11 @@ app.post('/register', async (req, res) => {
     }
 });
 
-
-// Verwende die Routen
+// Use the routes
 app.use('/', mensasRoute);
-console.log('middleware fuer mensa genutzt');
-/*app.use('/meals', mealsRoute);
-console.log('middleware fuer meals genutzt');
-app.use('/additives', additivesRoute);
-console.log('middleware fuer additives genutzt');
-*/
+// app.use('/meals', mealsRoute);
+// app.use('/additives', additivesRoute);
 
-const privateKey = fs.readFileSync('server.key','utf8');
-const certificate = fs.readFileSync('server.cert', 'utf8');
-const credentials = { key: privateKey, cert: certificate };
-
-const httpsServer = https.createServer(credentials, app);
-httpsServer.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server listening at port ${port}`);
 });
-/*const httpServer = app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
-});*/
-
